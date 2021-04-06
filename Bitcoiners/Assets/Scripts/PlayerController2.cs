a648b4905e61986b8e0e0defc698c58c576aa98f
xavi01 <xavier.navarro.mas@gmail.com>
2021-04-06 12:34:26 +0200

    movement

---
+++ Bitcoiners/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+    private int count;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+
+}
