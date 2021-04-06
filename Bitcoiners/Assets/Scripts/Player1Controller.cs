e13f1e5cf54c3241a5fe6e40b642104e2a375692
xavi01 <xavier.navarro.mas@gmail.com>
2021-04-06 13:10:42 +0200

    mover gitignore

--- Bitcoiners/Assets/Scripts/Player1Controller.cs
+++ Bitcoiners/Assets/Scripts/Player1Controller.cs
@@ -6,12 +6,17 @@ public class Player1Controller : MonoBehaviour
 {
     public float speed;             
     private Rigidbody2D rb2d;       
-    private int count;              
+    private int count;
+    private bool ball;
+    public GameObject pelota;
+    public GameObject player2;
 
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        ball = false;
+        pelota.SetActive(false);
     }
 
     void FixedUpdate()
@@ -24,6 +29,32 @@ public class Player1Controller : MonoBehaviour
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.S))
            transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (ball == true)
+        {
+            pelota.SetActive(true);
+        }
+
+        if (ball == false)
+        {
+            pelota.SetActive(false);
+        }
+    }
+
+
+    public void setBall()
+    {
+        ball = false;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player2"))
+        {
+            ball = true;
+            player2.gameObject.GetComponent<PlayerController2>().setBall();
+
+        }
     }
 
 }
--- Bitcoiners/Assets/Scripts/PlayerController2.cs
+++ Bitcoiners/Assets/Scripts/PlayerController2.cs
@@ -7,11 +7,15 @@ public class PlayerController2 : MonoBehaviour
     public float speed;
     private Rigidbody2D rb2d;
     private int count;
-
+    public GameObject pelota;
+    private bool ball = false;
+    public GameObject player1;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        ball = false;
+        pelota.SetActive(false);
     }
 
     void FixedUpdate()
@@ -24,6 +28,33 @@ public class PlayerController2 : MonoBehaviour
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.DownArrow))
             transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (ball == true)
+        {
+            pelota.SetActive(true);
+        }
+
+        if (ball == false)
+        {
+            pelota.SetActive(false);
+        }
+
     }
+
+
+    public void setBall() {
+        ball = false;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            ball = true;
+            player1.gameObject.GetComponent<Player1Controller>().setBall();
+
+        }
+    }
+
 
 }
