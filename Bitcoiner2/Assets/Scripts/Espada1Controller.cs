81528360fb70b99bf5b1705f2b3ecbecebf0b4c7
xavi01 <xavier.navarro.mas@gmail.com>
2021-04-08 11:08:27 +0200

    commit

---
+++ Bitcoiner2/Assets/Scripts/Espada1Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Espada1Controller : MonoBehaviour
+{
+
+    private int count;
+    private bool ball;
+    public GameObject pelota;
+    public GameObject player2;
+    public GameObject espada2;
+
+    void Start()
+    {
+        ball = false;
+        pelota.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+
+        if (ball == true)
+        {
+            pelota.SetActive(true);
+        }
+
+        if (ball == false)
+        {
+            pelota.SetActive(false);
+        }
+    }
+
+
+    public void setBall()
+    {
+        ball = false;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pelota2"))
+        {
+            ball = true;
+            espada2.gameObject.GetComponent<Espada1Controller>().setBall();
+
+        }
+
+        if (collision.gameObject.CompareTag("Pelota"))
+        {
+            ball = true;
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Bitcoiner2/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    public float speed;
+
+    public int velocity;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+           transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+           transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+    }
+
+}
